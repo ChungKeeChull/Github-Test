f2f6a48b7809c8780b5c879e80a2981dba94a9fb
정기철 <sirachung@hanmail.net>
2026-04-14 14:06:47 +0900

    initial upload test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int i = 0 ;
+	int sum = 0;
+	
+	for(i = 0; i <=10 ; i++ )
+	                   sum = sum+i ;
+
+    printf("sum = %d",sum);	                   
+	  
+	
+	
+}
